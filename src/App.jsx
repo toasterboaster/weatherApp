900f1afbeb683295bf8f690286977e77629b2f60
Josh <willJosh44@yahoo.com>
2023-08-29 20:28:04 +0000

    added extra cards with an array and put menu in the app file making card its own seprate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
 import React from 'react'
 import './App.css'
 import Start from './components/Start.jsx'
-import Menu from './components/Menu.jsx'
+import Card from './components/Card.jsx'
 import City from './components/City.jsx'
 
 
 export default function App() {
   const [start, setStart] = React.useState(false)
   const [cityCard, setCityCard] = React.useState(false);
-  const [data, setData] = React.useState(null);
+  const [data, setData] = React.useState([]);
   const [inputValue, setInputValue] = React.useState('');
 
-  const fetchData = () => {
-    fetch(`https://api.weatherapi.com/v1/current.json?key=c50302f0df1c4366907192905232408&q=${inputValue}`)
+  const fetchData = (zip) => {
+    fetch(`https://api.weatherapi.com/v1/current.json?key=c50302f0df1c4366907192905232408&q=${zip}`)
       .then(response => response.json())
-      .then((data) => {
+      .then((newData) => {
         console.log(data)
-        setData(data)})
+        setData(prevData => [...prevData, newData])
+      })
       .catch(error => console.error(error));
   };
 
 
   function startApp() {
     setStart(prev => !prev)
-    fetchData()
+    fetchData(inputValue)
   }
 
   function selectCityCard() {
@@ -35,17 +36,39 @@ export default function App() {
 
   }
 
+  function addAnotherCity(){
+    fetchData(inputValue)
+  }
+  
+  console.log(data)
+
+  let cityCardArray = data.map((city,index) => {
+    return (
+      <Card
+            cityName={city.location.name}
+            cityTemp={city.current.temp_f}
+            cityCondition={city.current.condition.text}
+            city={selectCityCard} 
+            />
+    )
+  })
+    
   return (
     <main>
       {!start && <Start start={startApp} inputValue={inputValue} handleInputChange={handleInputChange} />}
-      {start && !cityCard && data && <Menu
-        cityName={data.location.name}
-        cityTemp={data.current.temp_f}
-        cityCondition={data.current.condition.text}
-        city={selectCityCard} />}
-      {cityCard && <City 
-                     goBack={selectCityCard}
-                     cityName={data.location.name}/>}
+      {start && !cityCard && data &&
+        <div className='menu--container'>
+          {cityCardArray}
+          <div className='menu--card'>
+            <input placeholder="Enter city or zipcode" type="text" onChange={handleInputChange}></input>
+            <button className='add--card' onClick={addAnotherCity}>+</button>
+          </div>
+        </div>
+      }
+      {cityCard && <City
+        goBack={selectCityCard}
+        //cityName={data[0].location.name} 
+                     />}
     </main>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 
-export default function Menu(props) {
+export default function Card(props) {
   return (
-    <div className='menu--container'>
       <div className='menu--card'>
         <h3>{props.cityName}</h3>
         <h4>{props.cityTemp}</h4>
         <h4>{props.cityCondition}</h4>
         <button onClick={props.city}>More...</button>
       </div>
-      <div className='menu--card'>
-        <button className='add--card'>+</button>
-      </div>
-    </div>
   )
 }
