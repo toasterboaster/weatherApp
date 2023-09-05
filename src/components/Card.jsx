afa7cdabc9fb7042d90088440e2c002de4063914
Josh <willjosh44@yahoo.com>
2023-09-05 15:52:00 +0000

    added local time and fixed bug without inputValue to send alert

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,7 +14,9 @@ export default function Card(props) {
         <h3>{props.cityName}</h3>
         <h4>{props.cityTemp}</h4>
         <h4>{props.cityCondition}</h4>
+        <h4>{props.localTime}</h4>
         <h4>{props.id}</h4>
+        <h4>{props.inputValue}</h4>
         <button onClick={props.city}>More...</button>
       </div>
   )
